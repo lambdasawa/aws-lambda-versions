d0a8806fa736e9b9427922eca08a477b2e8cb675
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-02-21 16:07:05 +0900

    :mute: Remove debug log

--- app/nodejs/index.js
+++ app/nodejs/index.js
@@ -3,12 +3,6 @@ const AWS = require("aws-sdk");
 const bucketName = "aws-lambda-versions";
 
 exports.handler = async function (event, context) {
-  console.log({
-    Bucket: bucketName,
-    Key: process.env.AWS_EXECUTION_ENV,
-    Body: Buffer.from(process.version, "ascii"),
-  });
-
   await new AWS.S3()
     .putObject({
       Bucket: bucketName,
